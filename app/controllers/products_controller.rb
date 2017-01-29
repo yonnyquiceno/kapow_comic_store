7eec90863ebf978df1dbd23a287b3eb622c2bd63
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-01-29 18:51:43 -0500

    Add new product functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,24 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
   def new
+    @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:success] = 'comic registered successfully.'
+      redirect_to products_path
+    else
+      flash[:error] = @product.errors.full_messages.join(',')
+      render 'root'
+    end
+  end
+
+  private
+
+  def product_params
+    params[:product].permit(:name, :description, :price, :image1, :image2)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   get '/comics', to: 'products#index', as: 'products'
   get '/add-comics', to: 'products#new', as: 'new_product'
+  post '/add-comics', to: 'products#create', as: 'create_product'
 end
