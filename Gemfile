dcbe5c23455cf2c0995229e190ca8135e0b7195b
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-01-28 20:59:27 -0500

    devise initial config

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'fuzzy-string-match'
 gem 'faker'
+gem 'devise'
 group :development, :test do
   gem 'byebug'
 end
--- db/migrate/20170128231532_create_users.rb
+++ db/migrate/20170128231532_create_users.rb
@@ -3,9 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
       t.string :password
-
       t.timestamps null: false
     end
   end
