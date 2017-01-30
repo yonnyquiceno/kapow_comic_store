f1009ae2a078d620f1e4b0a94c9ff8f735154ddf
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-01-30 04:42:02 -0500

    Minor fixes

--- app/assets/javascripts/products_index.js
+++ app/assets/javascripts/products_index.js
@@ -6,7 +6,6 @@ function search(query) {
     }
 }
 $(document).ready(function() {
-    $("#search-field-container").hide()
     $(".show-search-field").click(function() {
         $("#search-field-container").show("slow")
     });
