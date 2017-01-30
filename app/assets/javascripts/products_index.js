5191dc4cb015b320312f159bd5527d149b84cdec
Alexander Quiceno <yonnyquiceno@gmail.com>
2017-01-30 02:54:32 -0500

    Minor changes

--- app/assets/javascripts/products_index.js
+++ app/assets/javascripts/products_index.js
@@ -1,7 +1,8 @@
 function search(query) {
     if (event.keyCode == 13) {
         q = query.value
-        window.location = "http://localhost:3000/comics?q=".concat(q.toString());
+        // window.location = "http://localhost:3000/comics?q=".concat(q.toString()); //development
+        window.location = "https://kapow-comic-store.herokuapp.com/comics?q=".concat(q.toString()); //production
     }
 }
 
